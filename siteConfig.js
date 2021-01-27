831eae22e24b2b2b59b382d0bdf6c7f14ae2d2e7
Mike Lasby <mklasby@ucalgary.ca>
2021-01-26 19:59:44 -0700

    pushing missing files

---
+++ siteConfig.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   name: 'London After Midnight',
+//   shortName: 'LAM',
+//   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+//   url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+//   prefix: '/',
+//   author: 'Vaporwavy',
+//   twitter: ''
+// }
+
+
+module.exports = {
+  name: 'Mike Lasby',
+  shortName: 'MKL',
+  description: "Mike's CV, portfolio, and blog!",
+  url: 'https://mikelasby.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Mike Lasby',
+  twitter: "",
+  instagram: 'https://www.instagram.com/mklasby/',
+  linkedIn: 'https://www.linkedin.com/in/mike-lasby-779b4388/',
+  github: 'https://github.com/mklasby'
+}
